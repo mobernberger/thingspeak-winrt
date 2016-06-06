e2291237823c63208ee8736fdeef2881c6f1addb
Daniel Ulfe <dulfe.github@outlook.com>
2016-06-05 21:10:05 -0400

    ThingSpeak return "0" when there was an errors updating a channel feed; Added code to support that. Added Exception to indicate an invalid response.

---
+++ ThingSpeakWinRT/Exceptions/InvalidResponseException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThingSpeakWinRT.Exceptions
+{
+    public class InvalidResponseException : Exception
+    {
+        public InvalidResponseException() : base() { }
+        public InvalidResponseException(string message) : base(message) { }
+        public InvalidResponseException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
